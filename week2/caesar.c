81dc308426f7c7753f1658cf7782cdb057dac9d9
Sebastian Wehmeyer <sebastianwehmeyer@Sebastians-MacBook-Pro.local>
2023-08-08 11:09:46 +0200

    caesar finished

--- week2/caesar.c
+++ week2/caesar.c
@@ -4,51 +4,68 @@
 #include <stdlib.h>
 
 // cc caesar.c -o caesar cs50.c
-char caesar(char[], int);
+char caesar(char, int);
 
 int main(int argc, string argv[]) 
 {
-   
-    if (argc == 2){
+    if (argc == 2)
+    {
         int key = atoi(argv[1]);
-        if (argc == 2 && key > 0) {
-            char new_code[100];
+        if (argc == 2 && key > 0)
+        {
+            char new_code[10000];
             string text = get_string("plaintext: ");
             char * code = text;
             printf("ciphertext: ");
-            new_code = caesar(code, key);
-            printf("%s", new_code);
-          /*  for (int n = 0; n < strlen(code); n++){  
-                printf("%c", caesar(code[n], key));
-            } */
 
-        } else {
+            for (int n = 0; n < strlen(code); n++)
+            {  
+                new_code[n] = caesar(code[n], key);
+            }
+
+            for (int k = 0; k < strlen(new_code); k++)
+            {
+                printf("%c", new_code[k]);
+            }
+           
+        printf("\n");
+
+        } else
+        {
             printf("./caesar key \n");
             return 1;
         }
 
-    } else {
-         printf("./caesar key \n");
-            return 1;
+    } else
+    {
+        printf("./caesar key \n");
+        return 1;
     }
-    
+
 }
 
-char caesar (char code_f[], int key_f) {
-    char new[10000];
-    for (int i = 0; i < strlen(code_f); i++) {
-        int rotate = code_f[i];
-        if (code_f[i] == ' ') {
-            new[i] = ' ';
-            i++;
-        } 
-        for (int j = 0; j < key_f; j++){
-            if (rotate == 90 || rotate == 122) {
+
+char caesar (char code_f, int key_f)
+{
+    char new;
+    int rotate = code_f;
+    if (code_f == ' ')
+    {
+        new = ' ';
+    } else
+    {
+        for (int j = 0; j < key_f; j++)
+        {
+            if (rotate == 90 || rotate == 122)
+            {
                 rotate -= 26;  
             }
+
             rotate++;
         }
-        new[i] = rotate;
+        new = rotate;
     }
-    return * new;
+
+    
+    return new;
 }
