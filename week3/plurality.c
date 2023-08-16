b46242c71203b5c646e5f4734902967dbf64b5d1
Sebastian Wehmeyer <sebastianwehmeyer@Sebastians-MacBook-Pro.local>
2023-08-16 14:53:46 +0200

    deleted cs50 comments, fixed styling and changed name of variables

--- week3/plurality.c
+++ week3/plurality.c
@@ -34,15 +34,15 @@ int main(int argc, string argv[])
         return 2;
     }
 
-    for (int i = 0; i < candidate_count; i++)
+    for (int candidate_number = 0; candidate_number < candidate_count; candidate_number++)
     {
-        candidates[i].name = argv[i + 1];
-        candidates[i].votes = 0;
+        candidates[candidate_number].name = argv[candidate_number + 1];
+        candidates[candidate_number].votes = 0;
     }
 
     int voter_count = get_int("Number of voters: ");
 
-    for (int i = 0; i < voter_count; i++)
+    for (int voter_number = 0;voter_number < voter_count; voter_number++)
     {
         string name = get_string("Vote: ");
 
@@ -57,11 +57,11 @@ int main(int argc, string argv[])
 
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int candidate_number = 0; candidate_number < candidate_count; candidate_number++)
     {
-        if (strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates[candidate_number].name, name) == 0)
         {
-            candidates[i].votes++;
+            candidates[candidate_number].votes++;
             return true;
         }
     }
@@ -71,24 +71,23 @@ bool vote(string name)
 
 void print_winner(void)
 {
-    int n = 0;
+    int max_votes = 0;
     string name;
 
-    for (int counter = 0; counter < candidate_count; counter++)
+    for (int candidate_number = 0; candidate_number < candidate_count; candidate_number++)
     {
-        if (candidates[counter].votes > n)
+        if (candidates[candidate_number].votes > max_votes)
         {
-            n = candidates[counter].votes;
+            max_votes = candidates[candidate_number].votes;
         }
     }
 
     for (int element = 0; element < candidate_count; element++)
     {
-        if (candidates[element].votes == n)
+        if (candidates[element].votes == max_votes)
         {
             printf("%s \n", candidates[element].name);
         }
-
     }
 
     return;
