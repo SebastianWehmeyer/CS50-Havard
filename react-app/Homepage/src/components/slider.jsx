8a0da699fa224eb2c63e024c75f5ae89025f78bc
Sebastian Wehmeyer <sebastianwehmeyer@Sebastians-MacBook-Pro.local>
2023-09-27 16:25:27 +0200

    extra slides

--- react-app/Homepage/src/components/slider.jsx
+++ react-app/Homepage/src/components/slider.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-import "../css/slider.css"
+import "../css/slider.css";
 
 const slideStyles = {
-    width: "250px",
-    height: "250px",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundColor: "green",
-    justifyContent: "center",
-    marginTop: "10px",
-    marginBottom: "25px",
-    marginLeft: "25px",
+  width: "250px",
+  height: "250px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundColor: "green",
+  justifyContent: "center",
+  marginTop: "10px",
+  marginBottom: "25px",
+  marginLeft: "25px",
 };
 
 const Slider = ({ slides }) => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
+  var currentIndexLower, currentIndexHigher;
 
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
@@ -38,6 +38,18 @@ const Slider = ({ slides }) => {
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
 
+  if (currentIndex == 0) {
+    currentIndexLower = 5;
+  } else {
+    currentIndexLower = currentIndex - 1;
+  }
+
+  if (currentIndex == 5) {
+    currentIndexHigher = 0;
+  } else {
+    currentIndexHigher = currentIndex + 1;
+  }
+
   return (
     <div className="sliderStyles">
       <div className="arrowContainer">
@@ -48,16 +60,39 @@ const Slider = ({ slides }) => {
           ❱
         </button>
       </div>
-      <div className="parent">
-        <fieldset>
+      <div className="socials">
+        <div id="parent-back-left">
+          <fieldset>
+            <legend>{slides[currentIndexLower].title}</legend>
+            <a href={slides[currentIndexLower].linkTo} target="_blank">
+              <img
+                src={slides[currentIndexLower].url}
+                className="slideStyles"
+              />
+            </a>
+          </fieldset>
+        </div>
+        <div id="parent-front">
+          <fieldset>
             <legend>{slides[currentIndex].title}</legend>
             <a href={slides[currentIndex].linkTo} target="_blank">
-                <img src={slides[currentIndex].url} className="slideStyles" />
+              <img src={slides[currentIndex].url} className="slideStyles" />
             </a>
-        </fieldset>
+          </fieldset>
+        </div>
+        <div className="parent-back-right">
+          <fieldset>
+            <legend>{slides[currentIndexHigher].title}</legend>
+            <a href={slides[currentIndexHigher].linkTo} target="_blank">
+              <img
+                src={slides[currentIndexHigher].url}
+                className="slideStyles"
+              />
+            </a>
+          </fieldset>
+        </div>
       </div>
-      
-      
+
       <div className="dotsContainerStyles">
         {slides.map((slide, slideIndex) => (
           <button
